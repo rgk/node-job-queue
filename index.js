08344b31e85466c06b961237fbd56a886c9e700c
Robert Kowalski <rgkmg25@gmail.com>
2022-11-26 23:56:51 -0500

    It no longer stops if an error happens.

--- index.js
+++ index.js
@@ -11,21 +11,25 @@ const output = new Map();
 
 +function keepGoing(start = 0) {
   if (~start) console.log([ ...output ]);
-  while (start++ < input.length) {
-    const inputFile = input[start];
-    processes.add(inputFile, execFile(inputFile, ['--queue'], (error, stdout, stderr) => {
-      if (error) throw error;
+    try {
+    while (start++ < input.length) {
+      const inputFile = input[start];
+      processes.add(inputFile, execFile(inputFile, ['--queue'], (error, stdout, stderr) => {
+        if (error) throw error;
 
-      if (stdout || stderr) output.set(inputFile, stdout || stderr);
+        if (stdout || stderr) output.set(inputFile, stdout || stderr);
 
-      console.log(`{$inputFile} is finished.`);
-      processes.delete(inputFile);
-      keepGoing(start < input.length ? start : -1);
-    }));
+        console.log(`{$inputFile} is finished.`);
+        processes.delete(inputFile);
+        keepGoing(start < input.length ? start : -1);
+      }));
 
-    console.log(`{$inputFile} is being processed.`);
+      console.log(`{$inputFile} is being processed.`);
 
-    if (processes.size >= THREAD_COUNT) break;
+      if (processes.size >= THREAD_COUNT) break;
+    }
+  } catch(err) {
+    console.error(err);
   }
 }();
 
