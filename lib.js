03e9de5aed2cab33195300efddf8d2dc797766e3
Robert Kowalski <rgkmg25@gmail.com>
2023-03-29 20:03:49 -0400

    Update lib.js

--- lib.js
+++ lib.js
@@ -4,31 +4,12 @@ import os from 'os';
 
 const THREAD_COUNT = os.cpus().length;
 
-const processes = new Map();
-const output = new Map();
-
-export function processJob(job) {
-  console.info(job + ' started.');
-  processes.set(job, execFile(job, ['--queue'], (error, stdout, stderr) => {
-    if (error) console.error(error);
-
-    if (stdout || stderr) output.set(job, stdout || stderr);
-
-    console.info(job + ' finished.');
-
-    processes.delete(job);
-
-    console.info(job + ' stopped.');
-
-    processJob();
-  }));
-}
-
 class JobQueue {
   constructor(list) {
     this.list = list;
     this.index = 0;
-    this.output = new Map();
+    this.threads = THREAD_COUNT;
+    this.output = [];
   }
 
   get list() {
@@ -38,14 +19,15 @@ class JobQueue {
   set list(list) {
     return this.list = list;
   }
+
   // Methods
   processJob(job) {
-    execFile(job, ['--queue'], (error, stdout, stderr) => {
+    return execFile(job, ['--queue'], (error, stdout, stderr) => {
       if (error) console.error(error);
 
-      if (stdout || stderr) this.output.set(job, stdout || stderr);
+      if (stdout || stderr) this.output[job] = stdout || stderr;
 
       console.info(job + ' finished.');
-    })
+    });
   }
 }
