690142aeafc22147df64683932611c3cbd37eb2a
Robert Kowalski <rgkmg25@gmail.com>
2023-04-09 17:29:20 -0400

    Update lib.js

--- lib.js
+++ lib.js
@@ -10,20 +10,29 @@ const THREAD_COUNT = os.cpus().length;
 class JobQueue {
   threads = THREAD_COUNT;
   output = [];
+
+  cwd = false;
+  timeout = false;
+  maxBuffer = false;
+
   constructor(list) {
     this.list = list;
   }
 
   // Methods
   processJob(job) {
-    return execFilePromise(job, ['--queue'], (error, stdout, stderr) => {
+    return execFilePromise(job, ['--queue'], {
+      ...this.cwd && { cwd: this.cwd },
+      ...this.timeout && { timeout: this.timeout },
+      ...this.maxBuffer && { maxBuffer: this.maxBuffer },
+    }, (error, stdout, stderr) => {
       if (error) console.error(error);
 
       if (stdout || stderr) this.output[job] = stdout || stderr;
     });
   }
 
-  *getJob() {
+  async *getJob() {
     for (let i = 0; i < this.list.length; i++) yield this.list[i];
   }
 
