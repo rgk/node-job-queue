4379da59b450022ee8c9e18d1bfe76779610d37a
Robert Kowalski <rgkmg25@gmail.com>
2023-04-17 17:14:55 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,31 +1,9 @@
-import { execFile } from 'child_process';
-
-import os from 'os';
-
-const THREAD_COUNT = os.cpus().length;
+import { JobQueue } from './lib.js';
 
 const [ runtime, file, ...input ] = process.argv;
 
-const processes = new Map();
-const output = new Map();
+const queue = new JobQueue(input);
 
-+function processJob(job = input.pop()) {
-  if (!job) return output;
+await queue.run();
 
-  console.info(job + ' started.');
-  processes.set(job, execFile(job, ['--queue'], (error, stdout, stderr) => {
-    if (error) console.error(error);
-
-    if (stdout || stderr) output.set(job, stdout || stderr);
-
-    console.info(job + ' finished.');
-
-    processes.delete(job);
-
-    processJob();
-  }));
-
-  if (processes.size <= THREAD_COUNT) processJob();
-}();
-
-console.info(`${runtime} ${file} is currently running.`);
+console.log(queue.result());
